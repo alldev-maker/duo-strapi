5a9bebc7695165ab884c25a95e3e90fc76f47096
topdev7 <EugeneNz99@gmail.com>
2022-10-01 12:26:30 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    // client: 'sqlite',
+    client: 'postgres',
+    connection: {
+      // filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      // ssl: {
+      //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      // },
+    },
+    debug: false,
+    useNullAsDefault: true,
+  },
+});
